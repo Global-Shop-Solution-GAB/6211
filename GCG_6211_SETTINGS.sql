eb9fed77d0f608380a829d9148b120b687891eab
Service Web <sw@gssmail.com>
2024-03-18 12:14:21 -0500

    Commit changes in C:\Websites\Git\emarlow\6211\GCG_6211_SETTINGS.sql

---
+++ GCG_6211_SETTINGS.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "GCG_6211_SETTINGS"(
+ "RemoteHost" CHAR(200),
+ "RemoteUser" CHAR(200),
+ "RemotePassword" CHAR(200),
+ "RemotePort" CHAR(200),
+ "Pickup_Path" CHAR(200),
+ "Dropoff_Path" CHAR(200),
+ "Proc_Path" CHAR(200),
+ "Error_Path" CHAR(200));
+
+
